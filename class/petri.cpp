57e31ef1437d61b50e09395abdb10383801d0e6a
Gabriela Moreira <gabrielamoreira05@gmail.com>
2018-11-07 10:03:08 -0200

    Net matrixes and transitions actions

--- class/petri.cpp
+++ class/petri.cpp
@@ -17,3 +17,71 @@ funcao(estado atual){
     return estado atual
 }
 */
+#include <iostream>
+#include <fstream>
+#include "petri.h"
+using namespace std;
+
+#define N_TRANSITIONS 18
+#define N_PLACES 17
+
+vector <vector <int>> read_matrix(string file){
+    vector <vector <int>> matrix;
+    matrix.resize(N_PLACES);
+    for (auto place : matrix)
+        place.resize(N_TRANSITIONS);
+
+    ifstream in(file);
+
+    if (!in.is_open()){
+        cout << file << " not found" << endl;
+        exit(1);
+    }
+
+    for (int i=0; i<N_PLACES; i++)
+        for (int j=0; j<N_TRANSITIONS; j++)
+            in >> matrix[i][j];
+
+    in.close();
+    return matrix;
+}
+
+
+vector <int> read_vector(string file){
+    vector <int> actions;
+    actions.resize(N_TRANSITIONS);
+
+    ifstream in(file);
+
+    if (!in.is_open()){
+        cout << file << " not found" << endl;
+        exit(1);
+    }
+
+    char ac;
+    int trem;
+
+    for (int i=0; i<N_TRANSITIONS; i++)
+        in >> ac >> trem;
+        switch(ac){
+            case 'N': actions[i] = N; break;
+            case 'R': actions[i] = R + (trem - 1); break;
+            case 'L': actions[i] = L + (trem - 1); break;
+            case 'S': actions[i] = S + (trem - 1); break;
+            case 'G': actions[i] = G + (trem - 1); break;
+
+        }
+
+    in.close();
+    return actions;
+}
+
+
+PetriNet build_net(){
+    PetriNet net;
+
+    net.pre = read_matrix("net/pre.txt");
+    net.pos = read_matrix("net/pos.txt");
+    net.actions = read_vector("net/actions.txt");
+
+}
--- class/petri.h
+++ class/petri.h
@@ -1,11 +1,29 @@
 #include <vector>
+#include <string>
 
-#ifndef PETRI_H
-#define PETRI_H
+using namespace std;
 
-class Transition{
+// #ifndef PETRI_H
+// #define PETRI_H
+
+#define N 0
+#define R1 1
+#define R2 2
+#define L1 3
+#define L2 4
+#define S1 5
+#define S2 6
+#define G0 7
+#define G1 8
+
+#define R 1
+#define L 3
+#define S 5
+#define G 7
+
+class PetriNet{
 public:
     vector <vector <int>> pre;
     vector <vector <int>> pos;
-    action();
-}
+    vector <int> actions;
+};
